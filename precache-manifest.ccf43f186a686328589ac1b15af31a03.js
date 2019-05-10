d769de0d5de048118113aea3378a07991fb95f48
chuyi <chuyingl@umich.edu>
2019-05-10 17:42:56 -0400

    Updates

---
+++ precache-manifest.ccf43f186a686328589ac1b15af31a03.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "77c6d5c37df62fccf3d8ee274eca436d",
+    "url": "/awesomeTravelPlanner/static/media/start-c6-marker.77c6d5c3.svg"
+  },
+  {
+    "revision": "c0ac34289bf1147be302",
+    "url": "/awesomeTravelPlanner/static/css/main.bfdceef7.chunk.css"
+  },
+  {
+    "revision": "43e6738fdf793baa32ec",
+    "url": "/awesomeTravelPlanner/static/js/runtime~main.1a3ff795.js"
+  },
+  {
+    "revision": "b80f5ca4d64f9d019912c21a2f21aa7b",
+    "url": "/awesomeTravelPlanner/static/media/c9-marker.b80f5ca4.svg"
+  },
+  {
+    "revision": "c25516fdf9f07abccb59",
+    "url": "/awesomeTravelPlanner/static/js/2.aacfbb23.chunk.js"
+  },
+  {
+    "revision": "772a46fcab4bec066c78a25ce6a0d6b5",
+    "url": "/awesomeTravelPlanner/static/media/NYCPage.772a46fc.png"
+  },
+  {
+    "revision": "1faad34a22727305bb6438c54a88e835",
+    "url": "/awesomeTravelPlanner/static/media/logo.1faad34a.png"
+  },
+  {
+    "revision": "f712dac3c383f2a1c70167207ba7babb",
+    "url": "/awesomeTravelPlanner/static/media/blue-marker.f712dac3.svg"
+  },
+  {
+    "revision": "633c13c2650d4fdd8cadf916c6abf224",
+    "url": "/awesomeTravelPlanner/static/media/black-marker.633c13c2.png"
+  },
+  {
+    "revision": "e03d61ae7c1a0173464a6b8a46367fb5",
+    "url": "/awesomeTravelPlanner/static/media/start-c1-marker.e03d61ae.svg"
+  },
+  {
+    "revision": "4777248791ce7f3e070f93658491f9a5",
+    "url": "/awesomeTravelPlanner/static/media/start-c10-marker.47772487.svg"
+  },
+  {
+    "revision": "74bdb13a8e1e03c14f9b0e047a2a06c7",
+    "url": "/awesomeTravelPlanner/static/media/start-c11-marker.74bdb13a.svg"
+  },
+  {
+    "revision": "389f3f3a2de8ae194bf6a411f8129982",
+    "url": "/awesomeTravelPlanner/static/media/start-c12-marker.389f3f3a.svg"
+  },
+  {
+    "revision": "60053aa5ce80cb19c6caf816d99be39c",
+    "url": "/awesomeTravelPlanner/static/media/start-c13-marker.60053aa5.svg"
+  },
+  {
+    "revision": "7f7fcd923aaa520724569f002156c436",
+    "url": "/awesomeTravelPlanner/static/media/start-c14-marker.7f7fcd92.svg"
+  },
+  {
+    "revision": "9bd95ce4d28784717057e0f6b4650930",
+    "url": "/awesomeTravelPlanner/static/media/start-c15-marker.9bd95ce4.svg"
+  },
+  {
+    "revision": "9279fb1666d223bb88f4765d7a5788ba",
+    "url": "/awesomeTravelPlanner/static/media/start-c2-marker.9279fb16.svg"
+  },
+  {
+    "revision": "eb898865e4aeeaad2729a61fcded506e",
+    "url": "/awesomeTravelPlanner/static/media/start-c3-marker.eb898865.svg"
+  },
+  {
+    "revision": "22f7ab1fe64c84d4ee7f59fd63407624",
+    "url": "/awesomeTravelPlanner/static/media/start-c4-marker.22f7ab1f.svg"
+  },
+  {
+    "revision": "0d3f342ad099896521852bb2bd109264",
+    "url": "/awesomeTravelPlanner/static/media/start-c5-marker.0d3f342a.svg"
+  },
+  {
+    "revision": "c0ac34289bf1147be302",
+    "url": "/awesomeTravelPlanner/static/js/main.5ff5f8c1.chunk.js"
+  },
+  {
+    "revision": "3629785bce72472da4043659572e034d",
+    "url": "/awesomeTravelPlanner/static/media/start-c7-marker.3629785b.svg"
+  },
+  {
+    "revision": "25f091526a6f67287c0e14b995765b40",
+    "url": "/awesomeTravelPlanner/static/media/start-c8-marker.25f09152.svg"
+  },
+  {
+    "revision": "f9f3977880ba74ec577d958535c9da4b",
+    "url": "/awesomeTravelPlanner/static/media/start-c9-marker.f9f39778.svg"
+  },
+  {
+    "revision": "6f6bdf7a460dee7303e7946f311e15ee",
+    "url": "/awesomeTravelPlanner/static/media/c1-marker.6f6bdf7a.svg"
+  },
+  {
+    "revision": "7de4a5d84c90d66868eccedac04011e3",
+    "url": "/awesomeTravelPlanner/static/media/c10-marker.7de4a5d8.svg"
+  },
+  {
+    "revision": "a38a812ac19163cbb1707f5357364076",
+    "url": "/awesomeTravelPlanner/static/media/c11-marker.a38a812a.svg"
+  },
+  {
+    "revision": "0404b29fc4ee2668ef0d9c533821729e",
+    "url": "/awesomeTravelPlanner/static/media/c12-marker.0404b29f.svg"
+  },
+  {
+    "revision": "6394b8880f90157663614bdaa16dca54",
+    "url": "/awesomeTravelPlanner/static/media/c13-marker.6394b888.svg"
+  },
+  {
+    "revision": "c457f34d416a8262ff63015c24fbaf5a",
+    "url": "/awesomeTravelPlanner/static/media/c14-marker.c457f34d.svg"
+  },
+  {
+    "revision": "b3cdbf4f67aa3c9e583652bad6b93d11",
+    "url": "/awesomeTravelPlanner/static/media/c15-marker.b3cdbf4f.svg"
+  },
+  {
+    "revision": "1a65b8e57e72dc2df89ebecccf0c48fe",
+    "url": "/awesomeTravelPlanner/static/media/c2-marker.1a65b8e5.svg"
+  },
+  {
+    "revision": "82bffb153b1ec1a8eea2e5a4cb48e612",
+    "url": "/awesomeTravelPlanner/static/media/c3-marker.82bffb15.svg"
+  },
+  {
+    "revision": "ed12a289e6142bbb2406777b3f79c59d",
+    "url": "/awesomeTravelPlanner/static/media/c4-marker.ed12a289.svg"
+  },
+  {
+    "revision": "4de0901188fa7fb3938f3b3613638258",
+    "url": "/awesomeTravelPlanner/static/media/c5-marker.4de09011.svg"
+  },
+  {
+    "revision": "f3d441fc67de875261e47cc0837e8c76",
+    "url": "/awesomeTravelPlanner/static/media/c6-marker.f3d441fc.svg"
+  },
+  {
+    "revision": "978d0e206966f024e5e059cbbdc125c4",
+    "url": "/awesomeTravelPlanner/static/media/c7-marker.978d0e20.svg"
+  },
+  {
+    "revision": "05bc5f503b3a224f477ea49abd306c2e",
+    "url": "/awesomeTravelPlanner/static/media/c8-marker.05bc5f50.svg"
+  },
+  {
+    "revision": "c25516fdf9f07abccb59",
+    "url": "/awesomeTravelPlanner/static/css/2.b568c97b.chunk.css"
+  },
+  {
+    "revision": "032dbcdf62ebf59f531875eb1f3a6801",
+    "url": "/awesomeTravelPlanner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/awesomeTravelPlanner/precache-manifest.2a7489514150a37562aadabd0754b4a5.js"
+  "/awesomeTravelPlanner/precache-manifest.ccf43f186a686328589ac1b15af31a03.js"
 );
 
 workbox.clientsClaim();
